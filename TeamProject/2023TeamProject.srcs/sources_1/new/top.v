dfcf83fb3b8e199d495c984110782c7d0da12dd5
HidenSong <bookmaru09@naver.com>
2023-11-23 13:25:41 +0900

    module

--- TeamProject/2023TeamProject.srcs/sources_1/new/top.v
+++ TeamProject/2023TeamProject.srcs/sources_1/new/top.v
@@ -1,9 +1,20 @@
 `timescale 1ns / 1ps
 
-module top( );
+module top( 
 
+    input clk, 
+    input rst,
 
+    input [9:0] x, 
+    input [9:0] y, 
+    
+    input [4:0] key, 
+    input [4:0] key_pulse, 
 
+    output [1:0] ball_rgb,
+    output [1:0] cue_rgb,
+    output font
+);
 
 
 endmodule
