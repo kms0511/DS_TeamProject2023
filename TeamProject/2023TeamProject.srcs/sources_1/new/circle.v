5d4612a231b04716d779d6940f205045d2a5a29c
TeddyElectronics <kyc20126@gmail.com>
2023-11-24 18:05:02 +0900

    Update circle.v

--- TeamProject/2023TeamProject.srcs/sources_1/new/circle.v
+++ TeamProject/2023TeamProject.srcs/sources_1/new/circle.v
@@ -4,8 +4,9 @@ input [9:0] cir_x, cir_y, cir_r,
 input [11:0] cir_rgb,
 output circle_on
     );
+wire distance;
 
-assign distance = (((cir_x-x)*(cir_x-x))+ ((cir_y-y)*(cir_y-y)));
+assign distance = (((cir_x-x)*(cir_x-x)) + ((cir_y-y)*(cir_y-y)));
 
 assign circle_on = (cir_r*cir_r >= distance)? 1 : 0; //circle이 있는 영역
 
